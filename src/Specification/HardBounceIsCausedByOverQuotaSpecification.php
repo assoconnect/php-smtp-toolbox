b62df0d89f7772320098416d463572f228650248
Sylvain Fabre <syl.fabre@gmail.com>
2022-10-05 13:54:29 +0200

    More specifications

---
+++ src/Specification/HardBounceIsCausedByOverQuotaSpecification.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\SmtpToolbox\Specification;
+
+class HardBounceIsCausedByOverQuotaSpecification
+{
+    private const NEEDLES = [
+        // Free: 552 5.2.2 user quota exceeded (UserSearch)
+        // Free: 554 5.2.2 <xxx@free.fr>: Recipient address rejected: Quota exceeded (mailbox for user is full)
+        '552 5.2.2 user quota exceeded (UserSearch)',
+        'Recipient address rejected: Quota exceeded (mailbox for user is full)',
+        // GMail: 4.2.2 The email account that you tried to reach is over quota.
+        '4.2.2 The email account that you tried to reach is over quota.',
+        // Orange: 552 5.1.1 g270oYR4MmRof Boite du destinataire pleine. Recipient overquota. OFR_417 [417]
+        'Recipient overquota',
+        // Senat.fr
+        'smtp; 452 4.2.2 Over quota',
+    ];
+
+    public function isSatisfiedBy(string $message): bool
+    {
+        foreach (self::NEEDLES as $needle) {
+            if (false !== strpos($message, $needle)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
--- src/Specification/HardBounceIsCausedByUnknownUserSpecification.php
+++ src/Specification/HardBounceIsCausedByUnknownUserSpecification.php
@@ -7,7 +7,16 @@ namespace AssoConnect\SmtpToolbox\Specification;
 class HardBounceIsCausedByUnknownUserSpecification
 {
     private const NEEDLES = [
-        'Sender user unknown', // SFR
+        // GMail: 5.1.1 The email account that you tried to reach does not exist.
+        '5.1.1 The email account that you tried to reach does not exist.',
+        // Orange: 5.1.1 <xxx@orange.com>: Recipient address rejected: User unknown
+        // Orange: 550 5.1.1 g1aPogOQcUQ7x Adresse d au moins un destinataire invalide. Invalid recipient. OFR_416 [416]
+        'Recipient address rejected: User unknown',
+        'Adresse d au moins un destinataire invalide. Invalid recipient. OFR_416 [416]',
+        // SFR:
+        'Sender user unknown',
+        // Yahoo: 1 Requested mail action aborted, mailbox not found
+        'Requested mail action aborted, mailbox not found',
     ];
 
     public function isSatisfiedBy(string $message): bool
