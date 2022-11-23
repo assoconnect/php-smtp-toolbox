da278ebbdfdecb022e1400954b2dc69abe3b33de
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2022-11-23 14:26:59 +0100

    Use PHPMailer & test for TLS

--- src/Exception/SmtpConnectionRuntimeException.php
+++ src/Exception/SmtpConnectionRuntimeException.php
@@ -6,4 +6,18 @@ namespace AssoConnect\SmtpToolbox\Exception;
 
 class SmtpConnectionRuntimeException extends \RuntimeException
 {
+    /** @param array<string, string> $smtpError */
+    public static function createFromSmtpError(string $command, array $smtpError): self
+    {
+        return new self(
+            sprintf(
+                '%s failed: %s (%s - %s)',
+                $command,
+                $smtpError['error'],
+                $smtpError['smtp_code'],
+                $smtpError['smtp_code_ex']
+            ),
+            (int)$smtpError['smtp_code']
+        );
+    }
 }
