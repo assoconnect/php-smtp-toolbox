bc318e14f4f780e3224df176e80d168c470d6f8c
Tristan Pouliquen <tristan.pouliquen@assoconnect.com>
2022-11-24 14:03:16 +0100

    Update SmtpValidatorInterface.php

--- src/Validation/SmtpValidatorInterface.php
+++ src/Validation/SmtpValidatorInterface.php
@@ -3,10 +3,11 @@
 namespace AssoConnect\SmtpToolbox\Validation;
 
 use AssoConnect\SmtpToolbox\Dto\ValidationStatusDtoInterface;
+use AssoConnect\SmtpToolbox\Exception\SmtpRuntimeConnectionException;
 use AssoConnect\SmtpToolbox\Exception\SmtpTemporaryFailureException;
 
 interface SmtpValidatorInterface
 {
-    /** @throws SmtpTemporaryFailureException */
+    /** @throws SmtpTemporaryFailureException|SmtpRuntimeConnectionException */
     public function validate(string $email): ValidationStatusDtoInterface;
 }
