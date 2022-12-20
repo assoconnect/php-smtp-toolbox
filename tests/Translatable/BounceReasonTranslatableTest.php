c6c09a56c78a6ca85eb899c91702b44809b9fc61
Florian Guimier <florian.guimier@assoconnect.com>
2022-12-20 12:06:31 +0100

    Add BounceReasonTranslatableTest

---
+++ tests/Translatable/BounceReasonTranslatableTest.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace AssoConnect\SmtpToolbox\Tests\Translatable;
+
+use AssoConnect\SmtpToolbox\Translatable\BounceReasonTranslatable;
+use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
+use Symfony\Contracts\Translation\TranslatorInterface;
+
+class BounceReasonTranslatableTest extends KernelTestCase
+{
+    /**
+     * @dataProvider providerTrans
+     */
+    public function testBounceReasonTranslatable(string $reason, string $expectedTranslationKey): void
+    {
+        self::bootKernel();
+
+        /** @var TranslatorInterface $translator */
+        $translator = self::getContainer()->get(TranslatorInterface::class);
+
+        self::assertSame(
+            $translator->trans($expectedTranslationKey, [], 'assoconnect_smtp_toolbox'),
+            (new BounceReasonTranslatable($reason))->trans($translator)
+        );
+    }
+
+    /**
+     * @return iterable<mixed>
+     */
+    public function providerTrans(): iterable
+    {
+        yield 'code' => [
+            '2.2.1 test code',
+            'bounce.reason.none'
+        ];
+
+        yield 'text' => [
+            'Not delivering to a user who marked your messages as spam',
+            'bounce.reason.spam'
+        ];
+
+        yield 'unknown' => [
+            'value does not exists',
+            'bounce.reason.none'
+        ];
+    }
+}
